9fd8c53c81414c4c9dbbed9f9d17d5a41fac523b
Rafael Metractive <rafael.ferreira@metractive.com>
2018-06-03 14:20:56 -0300

    styled images

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, StatusBar, StyleSheet, Text } from 'react-native'
+import { View, StatusBar, StyleSheet, Text, Image } from 'react-native'
 import {
     Container,
     Header,
@@ -7,6 +7,15 @@ import {
     Title,
     Content
 } from 'native-base';
+
+import styled from "styled-components";
+
+const IMAGES = {
+    DAREDEVIL: require('../../assets/images/devil.png'),
+    HULK_RION:  require('../../assets/images/hulk-rion.png'),
+    VENON_SPIDERMAN:  require('../../assets/images/venon-spiderman.png'),
+    WOLVERINE_CHAOS:  require('../../assets/images/wolverine-chaos.png')
+}
 
 let styles = StyleSheet.create({
     header: {
@@ -23,21 +32,32 @@ let styles = StyleSheet.create({
         flex: 1,
         alignItems: 'flex-end',
         justifyContent: 'flex-end',
-        height: 170,
+        minHeight: 170,
+        width: undefined,
         marginBottom: 8,
         marginHorizontal: 8,
         backgroundColor: '#000',
+        elevation: 4
     },
 
     menuItemText: {
-        marginRight: 8,
-        marginBottom: 8,
+        marginRight: 16,
+        marginBottom: 16,
         color: '#fff',
         fontSize: 36,
         fontWeight: 'bold',
-        alignSelf: 'flex-end'
+        alignSelf: 'flex-end',
+        zIndex: 2
     }
 })
+
+const StyledMenuImage = styled.Image`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    opacity: 0.8;
+`;
 
 class Home extends Component {
     render() {
@@ -56,15 +76,31 @@ class Home extends Component {
                 <Content style={{flexDirection: 'column', backgroundColor: '#fff'}}>
 
                     <View style={[styles.menuItemContainer, {marginTop: 8}]}>
+                        <StyledMenuImage
+                            source={IMAGES.HULK_RION}>
+                        </StyledMenuImage>
                         <Text style={styles.menuItemText}>CHARACTERS</Text>
                     </View>
 
                     <View style={styles.menuItemContainer}>
+                        <StyledMenuImage
+                            source={IMAGES.DAREDEVIL}>
+                        </StyledMenuImage>
                         <Text style={styles.menuItemText}>COMICS</Text>
                     </View>
 
                     <View style={styles.menuItemContainer}>
-                            <Text style={styles.menuItemText}>CREATORS</Text>
+                        <StyledMenuImage
+                            source={IMAGES.VENON_SPIDERMAN}>
+                        </StyledMenuImage>
+                        <Text style={styles.menuItemText}>CREATORS</Text>
+                    </View>
+
+                    <View style={styles.menuItemContainer}>
+                        <StyledMenuImage
+                            source={IMAGES.WOLVERINE_CHAOS}>
+                        </StyledMenuImage>
+                        <Text style={styles.menuItemText}>EVENTS</Text>
                     </View>
 
                 </Content>
